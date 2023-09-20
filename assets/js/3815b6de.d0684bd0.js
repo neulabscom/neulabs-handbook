60e5a0337ec5fa74dd7807e2f8e386befe174501
FabrizioCafolla <FabrizioCafolla@users.noreply.github.com>
2023-09-20 15:48:12 +0000

    deploy: 29e2bbf36ae6741142a7afa37e3e513c911fc8c2

---
+++ assets/js/3815b6de.d0684bd0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[818],{8427:e=>{e.exports=JSON.parse('{"title":"Organization","keywords":["guides"],"slug":"/organization","permalink":"/organization","navigation":{"previous":{"title":"\u2764\ufe0f The Neulabs Handbook","permalink":"/"},"next":{"title":"Overview","permalink":"/organization/overview"}}}')}}]);
